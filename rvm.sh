0140a4cc33ee0149a076fb11f02ff2a9d6b20e6a
Michael Vitz <vitz.michael@googlemail.com>
2013-03-24 15:33:05 +0100

    Disabled RVM module.

--- rvm.sh
+++ rvm.sh
@@ -1,8 +1,10 @@
 # rvm related stuff
 #
 
-export RVM_HOME=~/.rvm
-PATH=$PATH:$RVM_HOME/bin
-
-[[ -s $RVM_HOME/scripts/rvm ]] && . $RVM_HOME/scripts/rvm
-[[ -s $RVM_HOME/scripts/completion ]] && . $RVM_HOME/scripts/completion
+#export RVM_HOME=~/.rvm
+#PATH=$PATH:$RVM_HOME/bin
+#
+#[[ -s $RVM_HOME/scripts/rvm ]] && . $RVM_HOME/scripts/rvm
+#[[ -s $RVM_HOME/scripts/completion ]] && . $RVM_HOME/scripts/completion
+#
+#complete -F _gem18 gem
