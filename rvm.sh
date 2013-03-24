5d46697fb224406c75397361cb0aec70c9fc2bc9
Michael Vitz <vitz.michael@googlemail.com>
2013-03-24 14:18:34 +0100

    Removed login shell mimic from RVM module.

--- rvm.sh
+++ rvm.sh
@@ -4,7 +4,5 @@
 export RVM_HOME=~/.rvm
 PATH=$PATH:$RVM_HOME/bin
 
-if shopt -q login_shell; then
-  [[ -s $RVM_HOME/scripts/rvm ]] && . $RVM_HOME/scripts/rvm
-  [[ -s $RVM_HOME/scripts/completion ]] && . $RVM_HOME/scripts/completion
-fi
+[[ -s $RVM_HOME/scripts/rvm ]] && . $RVM_HOME/scripts/rvm
+[[ -s $RVM_HOME/scripts/completion ]] && . $RVM_HOME/scripts/completion
