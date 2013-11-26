dd5395a20974f972ca8e93fa7fbc5cd5be68f9c2
Michael Vitz <vitz.michael@googlemail.com>
2013-11-26 21:07:05 +0100

    Link ~/.dotfiles is now created in install script.

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 export dotfiles_root=$(cd "$(dirname "$0")"; pwd -P)
 export dotfiles_scripts=$dotfiles_root/scripts
 
+ln -s $dotfiles_root ~/.dotfiles
+
 for install_script in $dotfiles_root/install/*.sh; do
   $install_script
 done
