928afabd834ba0367b2c972fbb5f870a0492d850
Michael Vitz <vitz.michael@googlemail.com>
2013-12-04 22:17:24 +0100

    vim: add pathogen and gradle plugin

--- install/120-setup-vim.sh
+++ install/120-setup-vim.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 echo "Setting up vim"
+
 "$dotfiles_scripts/install-dotfile.sh" "$dotfiles_root/vim/vimrc" ~/.vimrc
+
+mkdir ~/.vim
+"$dotfiles_scripts/install-dotfile.sh" "$dotfiles_root/vim/pathogen/autoload" ~/.vim/autoload
+"$dotfiles_scripts/install-dotfile.sh" "$dotfiles_root/vim/bundle" ~/.vim/bundle
