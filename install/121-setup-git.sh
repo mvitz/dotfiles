a3080d0326d0f09fae5cc7c6f6c9f1570573dc97
Michael Vitz <michael.vitz@innoq.com>
2015-03-03 10:17:15 +0100

    git: add global gitignore

--- install/121-setup-git.sh
+++ install/121-setup-git.sh
@@ -2,3 +2,4 @@
 
 echo "Setting up git"
 "$dotfiles_scripts/install-dotfile.sh" "$HOME/.dotfiles/git/gitconfig" ~/.gitconfig
+"$dotfiles_scripts/install-dotfile.sh" "$HOME/.dotfiles/git/gitignore" ~/.gitignore
