3382603bbe05305c416a819eeab8eb595ed66baf
Michael Vitz <vitz.michael@googlemail.com>
2016-07-01 14:14:18 +0200

    git: use gitcommit template

--- install/121-setup-git.sh
+++ install/121-setup-git.sh
@@ -3,3 +3,4 @@
 echo "Setting up git"
 "$dotfiles_scripts/install-dotfile.sh" "$HOME/.dotfiles/git/gitconfig" ~/.gitconfig
 "$dotfiles_scripts/install-dotfile.sh" "$HOME/.dotfiles/git/gitignore" ~/.gitignore
+"$dotfiles_scripts/install-dotfile.sh" "$HOME/.dotfiles/git/gitcommit" ~/.gitcommit
