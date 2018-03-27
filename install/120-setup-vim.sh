53154d7ea05ef91056787fca9fd7157f775ed673
Michael Vitz <vitz.michael@googlemail.com>
2018-03-27 13:08:00 +0200

    vim: add gvimrc

--- install/120-setup-vim.sh
+++ install/120-setup-vim.sh
@@ -3,6 +3,7 @@
 echo "Setting up vim"
 
 "$dotfiles_scripts/install-dotfile.sh" "$HOME/.dotfiles/vim/vimrc" ~/.vimrc
+"$dotfiles_scripts/install-dotfile.sh" "$HOME/.dotfiles/vim/gvimrc" ~/.gvimrc
 
 mkdir ~/.vim
 "$dotfiles_scripts/install-dotfile.sh" "$HOME/.dotfiles/vim/pathogen/autoload" ~/.vim/autoload
