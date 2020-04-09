f4b2bb607e991c2ee5b8ffaa897249c4a8d276fb
Michael Vitz <vitz.michael@googlemail.com>
2020-04-09 11:04:18 +0200

    git: Add initial gitconfig

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+DOTFILES_REPO_DIR=$(cd "$(dirname "$0")"; pwd -P)
+DOTFILES_DIR="${HOME}/.dotfiles"
+
+ln_dotfile() {
+  local dotfile="${1}"
+  [[ -L "${HOME}/.${dotfile}" ]] && unlink "${HOME}/.${dotfile}"
+  ln -s "${DOTFILES_DIR}/${dotfile}" "${HOME}/.${dotfile}"
+}
+
+# create symlink for dotfiles
+[[ -L "${DOTFILES_DIR}" ]] && unlink "${DOTFILES_DIR}"
+ln -s "${DOTFILES_REPO_DIR}" "${DOTFILES_DIR}"
+
+# git
+ln_dotfile gitconfig
