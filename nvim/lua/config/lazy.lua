cf7658b7b6d832adcafb058db9e4e073d33d4574
Michael Vitz <vitz.michael@googlemail.com>
2025-11-17 13:48:02 +0100

    neovim: Change local leader to space

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -26,4 +26,4 @@ vim.opt.rtp:prepend(lazypath)
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+vim.g.maplocalleader = " "
