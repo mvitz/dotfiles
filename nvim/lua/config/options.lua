efe564421e18bca015289f5dced8b4f8ad61cf72
Michael Vitz <vitz.michael@googlemail.com>
2026-02-17 15:31:34 +0100

    neovim: Add basic scrolling related options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -21,3 +21,7 @@ vim.opt.shiftwidth = 2 -- use two spaces for indent
 vim.opt.smartindent = true -- use C syntax like rules for indentation
 vim.opt.softtabstop = 2 -- display tab as two spaces during editing
 vim.opt.tabstop = 2 -- display tab as two spaces
+
+-- SCROLLING
+vim.opt.sidescrolloff = 11 -- start scrolling right/left when we're 11 columns away from margins
+vim.opt.scrolloff = 8 -- start scrolling up/down when we're 8 lines away from margins
