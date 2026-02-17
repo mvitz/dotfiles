173fb804024ece83c5e2ea9eecc04d34f50aca76
Michael Vitz <vitz.michael@googlemail.com>
2026-02-17 15:26:02 +0100

    neovim: Add basic UI related options

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,12 @@
+-- UI
+--vim.opt.background = "light"
+vim.opt.colorcolumn = "+1" -- show colored margin at textwidth
+vim.opt.cursorline = true -- enable highlighting of the current line
+vim.opt.list = true -- show hidden characters
+vim.opt.listchars = {
+  tab = "»·", -- show »· for tab
+  trail = "·", -- show · for trailing spaces
+  nbsp = "␣", -- show ␣ for non breaking spaces
+}
+vim.opt.number = true -- show line numbers
+vim.opt.relativenumber = true -- show relative line numbers
