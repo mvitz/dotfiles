337910994f4238ab5e7a60b1c78c083380728def
Michael Vitz <vitz.michael@googlemail.com>
2026-02-17 15:39:31 +0100

    neovim: Disable arrow keys

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,5 @@
+-- disable arrow keys
+vim.keymap.set("", "<Up>", "<NOP>") -- disable Up
+vim.keymap.set("", "<Right>", "<NOP>") -- disable Right
+vim.keymap.set("", "<Down>", "<NOP>") -- disable Down
+vim.keymap.set("", "<Left>", "<NOP>") -- disable Left
