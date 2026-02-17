bfcaeb78c6db2577d163cf4bfb50a30b9d6d18a2
Michael Vitz <vitz.michael@googlemail.com>
2026-02-17 15:31:03 +0100

    neovim: Add basic indentation related options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -14,3 +14,10 @@ vim.opt.relativenumber = true -- show relative line numbers
 -- SEARCH
 vim.opt.ignorecase = true -- ignore case when searching
 vim.opt.smartcase = true -- don't ignore case when pattern contains an upper-case character
+
+-- INDENTATION
+vim.opt.expandtab = true -- use spaces instead of tab
+vim.opt.shiftwidth = 2 -- use two spaces for indent
+vim.opt.smartindent = true -- use C syntax like rules for indentation
+vim.opt.softtabstop = 2 -- display tab as two spaces during editing
+vim.opt.tabstop = 2 -- display tab as two spaces
