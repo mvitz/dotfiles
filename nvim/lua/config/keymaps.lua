92a399c51123692b7a7d5b06f6b3c6af604f6f57
Michael Vitz <vitz.michael@googlemail.com>
2026-02-17 15:40:18 +0100

    neovim: Enter command mode with single keystroke

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,3 +3,7 @@ vim.keymap.set("", "<Up>", "<NOP>") -- disable Up
 vim.keymap.set("", "<Right>", "<NOP>") -- disable Right
 vim.keymap.set("", "<Down>", "<NOP>") -- disable Down
 vim.keymap.set("", "<Left>", "<NOP>") -- disable Left
+
+-- enter command mode with single keystroke
+vim.keymap.set("n", ";", ":") -- map ; to : in normal mode
+vim.keymap.set("n", ":", ";") -- map : to ; in normal mode
