6391b5337699c407a92f00986fbf1c2ddc2635c5
Michael Vitz <vitz.michael@googlemail.com>
2026-02-25 11:52:38 +0100

    neovim: Extend lua support

--- nvim/lua/plugins/lang/lua.lua
+++ nvim/lua/plugins/lang/lua.lua
@@ -69,4 +69,15 @@ return {
       },
     },
   },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    cmd = "LazyDev",
+    opts = {
+      library = {
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        { path = "snacks.nvim", words = { "Snacks" } },
+      },
+    },
+  },
 }
